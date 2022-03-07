f42adf420d1c3bf8c7bffadaf5e89291ee94e56e
adi <daw09.2022@gmail.com>
2022-03-07 13:26:04 +0100

    Proyecto documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,11 +1,19 @@
 
 package examen;
 
-
+/**
+ * Clase con el metodo para comprobar si la hora introcida es valida.
+ */
 public class Hora {
     public static boolean horaValida;
 
-
+    /**
+     *
+     * @param segundos Segundos a introducir que no sea <0
+     * @param minutos Minutos a introducir que no sean <0
+     * @param horas Horas a introducir que no sean <0
+     * @return Return false o true en funcion de si los datos introducidos son correctos
+     */
     public static boolean validarHora(int segundos, int minutos, int horas) {
         horaValida = true;
         if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
--- src/examen/HoraTest.java
+++ src/examen/HoraTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+/**
+ * Clase con valores de prueba
+ */
 public class HoraTest {
 
 
