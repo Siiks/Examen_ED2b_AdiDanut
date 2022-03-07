dbe0ff4299be1e58f59453b594eccc358fc7455c
adi <daw09.2022@gmail.com>
2022-03-07 13:25:02 +0100

    Proyecto sin documentar

---
+++ src/examen/HoraTest.java
@@ -0,0 +1,36 @@
+package examen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class HoraTest {
+
+
+    @Test
+    void CP1() {
+        Hora x = new Hora();
+        assertFalse(x.validarHora(5, 5, -1));
+
+    }
+    @Test
+    void CP2() {
+        Hora x = new Hora();
+        assertFalse(x.validarHora(5, -5, 5));
+
+    }
+    @Test
+    void CP3() {
+        Hora x = new Hora();
+        assertFalse(Hora.validarHora(-5, 5, 5));
+
+    }
+    @Test
+    void CP4() {
+        Hora x = new Hora();
+        assertTrue(x.validarHora(5, 5, 5));
+
+    }
+}
